1610f1769369338e62ad8f03cd3e4f3130d0f7ad
Edison <premusic314@gmail.com>
2025-12-14 16:50:40 -0500

    roles

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 nest generate module usuarios
 nest generate controller usuarios
 nest generate service usuarios
+
+nest generate module roles
+nest generate controller roles
+nest generate service roles
+
+nest generate module perfil-clientes
+nest generate controller perfil-clientes
+nest generate service perfil-clientes
+
+nest generate module orden-compras
+nest generate controller orden-compras
+nest generate service orden-compras
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { UsuariosModule } from './usuarios/usuarios.module';
       // ssl: { rejectUnauthorized: false },
     }),
     UsuariosModule,
+    RolesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
