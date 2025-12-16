58b3efafb777336b30a1f1d28dcbc50d4de75d63
Adavidfr <adavidfruiz@gmail.com>
2025-12-15 23:22:55 -0500

    creacion de servicio, controlador y modulo de auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FacturaModule } from './factura/factura.module';
 import { DetalleFacturaModule } from './detalle_factura/detalle_factura.module';
 import { KardexModule } from './kardex/kardex.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [FacturaModule, DetalleFacturaModule, KardexModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      // ssl: { rejectUnauthorized: false },
+    }),
+    FacturaModule,
+    DetalleFacturaModule,
+    KardexModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
